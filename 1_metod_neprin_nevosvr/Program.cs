9e0150eeaa045837f09c014d0bd0dd81cab2d042
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-11 22:41:37 +0100

    metod21

--- 1_metod_neprin_nevosvr/Program.cs
+++ 1_metod_neprin_nevosvr/Program.cs
@@ -3,7 +3,7 @@ void Method1()
 {
     Console.WriteLine("Autor Smolin Evdokim");
 }
-Method1(); // tak visivaetsia etot vid funkzii obiazatelno ()
+//Method1(); // tak visivaetsia etot vid funkzii obiazatelno ()
 
 
 //vid 2 - tolko prinimaet argumenti
@@ -24,3 +24,31 @@ void Method21(string msg, int count)
 }
 //Method21("Text.....", 4);
 //Method21(count: 4, msg: "neu Text");
+
+//3 vid - vosvras4aet
+
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+//Console.WriteLine(year);
+
+
+// vid 4 prinimaiut i vosvras4aiut
+
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+
+}
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
